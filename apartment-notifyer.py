a40c29b0c73f7ce3f3b698828a311b59b700cd21
Henrik Engström <me@henrikengstrom.com>
2019-11-06 20:36:38 +0100

    First commit!

---
+++ apartment-notifyer.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import os
+import requests
+from lxml import html
+from pathlib import Path
+from pushover import init, Client
+from datetime import datetime
+
+# Config
+_TOKEN = "<pushover-token>"
+_KEY = "<pushover-key>"
+
+# End of config
+date = datetime.now().strftime("%Y-%m-%d")  # Eg. 2019-10-23
+log_file = "{}/notifications.log".format(Path(os.path.realpath(__file__)).parent)
+
+def log(message):
+    f = open(log_file, "a+")
+    f.write("{}\n".format(message))
+    f.close()
+
+# Scrape HTML
+page = requests.get('https://wahlinfastigheter.se/lediga-objekt/lagenheter/')
+tree = html.fromstring(page.content)
+
+apartments_found = int(tree.xpath('//span[@class="total-article"]/text()')[1])  # Int
+type = tree.xpath('//span[@class="term-name"]/text()')[1]                       # Förråd, Lägenheter, Lokaler, P-platser
+
+print(u"{} {}".format(apartments_found, type))
+
+# Logic - send push or not
+try:
+    last_run = None
+    last_run_apartments_found = None
+
+    # Create log
+    if os.path.exists(log_file) == False:
+        log("date;apartments_found")
+        log("{};{}".format(date, apartments_found))
+
+    # Read last line in log
+    with open(log_file, 'r') as f:
+        lines = f.read().splitlines()
+        last_run = lines[-1][:10]
+        last_run_apartments_found = int(lines[-1][11:])
+
+    # Exit if push has been sent today
+    if date != last_run:
+        log("{};{}".format(date, apartments_found))
+
+    if date == last_run and apartments_found <= last_run_apartments_found:
+        print("Nothing to push.")
+        os._exit(0)
+except:
+    print("An error was thrown")
+
+# Logic - send push or not
+if apartments_found > 0:
+    # Pushover
+    init(_TOKEN)
+    Client(_KEY).send_message(u"Intresseanmäl på https://wahlinfastigheter.se/lediga-objekt/lagenheter/"
+                              .format(apartments_found), title="{} nya lägenheter".format(apartments_found))
+
+    print("Pushover message sent")
+    log("{};{}".format(date, apartments_found))
